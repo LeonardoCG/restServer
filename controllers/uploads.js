fab9e5f0f9fba0edb7679608fbdc63412df44f5a
LeoardoCG <andresxach7@gmail.com>
2022-12-09 00:02:00 -0600

    uploads cloudinary

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const fs = require('fs');
 
+const cloudinary = require('cloudinary').v2;
+
+//configurar y autentificar con cloudinary
+cloudinary.config( process.env.CLOUDINARY_URL);
+
 const { response } = require("express");
 const { subirArchivo } = require("../helpers/subir-Archivo");
 const { Usuario, Producto} = require("../models");
@@ -109,8 +114,61 @@ const mostrarImagen = async(req, res = response ) => {
     return res.sendFile( pathPlaceHolder);
 }
 
+const actualizarImagenCloudinary = async(req, res = response) => {
+
+    const { id, coleccion } = req.params;
+
+    let modelo;
+
+    switch ( coleccion ) {
+        case 'usuarios':
+            modelo = await Usuario.findById(id);
+            if( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un usuario con el id ${ id }`
+                });
+            }
+            break;
+        
+        case 'productos':
+            modelo = await Producto.findById(id);
+            if( !modelo ) {
+                return res.status(400).json({
+                    msg: `No existe un producto con el id ${ id }`
+                });
+            }
+            break;
+    
+        default:
+            return res.status(500).json({ msg: 'Se me olvido'});
+    }
+    //limpiar imagenes previas en cloudinary
+    if( modelo.img ) {
+        //separamos por un slash
+        const nombreArr = modelo.img.split('/');
+        //tomamos el ultimo texto o posicion 
+        const nombre = nombreArr[ nombreArr.length - 1 ];
+        //cortamos donde encontramos el punto y guardamos en public_id
+        const [ public_id ] = nombre.split('.');
+        //borrar de cloudinary en un proceso aparte
+        cloudinary.uploader.destroy( public_id );
+    }
+
+    //subir img a cloudinary
+    //desestructurar y extraer el path temporal
+    const { tempFilePath } = req.files.archivo;
+    //desestructurar y extraer la url segura
+    const { secure_url } = await cloudinary.uploader.upload( tempFilePath );   
+    modelo.img = await secure_url; 
+    //guardamos la url
+    await modelo.save();
+
+    res.json( modelo );
+}
+
 module.exports = {
     cargarArchivo,
     actualizarImagen,
-    mostrarImagen
+    mostrarImagen,
+    actualizarImagenCloudinary
 } 
